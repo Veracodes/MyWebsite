52d07eacd362185cca94d9dc2f0b967fff98a7c4
Veracodes <71265252+Veracodes@users.noreply.github.com>
2022-03-20 12:35:38 +0000

    Loader with animation added

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+// Loader
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+  
