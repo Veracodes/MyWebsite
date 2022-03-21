17bae2f898aa40ad93ec9ccf53f7b865a757b379
Veracodes <71265252+Veracodes@users.noreply.github.com>
2022-03-21 12:16:53 +0000

    Hamburger menu collapse fixed
    Added jQuery function to collapse side nav bar when clicked anywhere outside of it

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,4 +9,15 @@ window.addEventListener("load", () => {
       document.body.removeChild(loader);
     });
   });
-  
+
+  // nav bar collapse on small screens when clicked outside nav bar
+
+  jQuery(function(){
+    $( '#hamburger-menu' ).click(function(){
+       $('#sidebar-menu').toggleClass('expand');
+       $('.overlay').toggleClass('open');            
+    });  
+    $('.overlay').click(function(){
+    $('#hamburger-menu').trigger('click');
+    });
+  });
